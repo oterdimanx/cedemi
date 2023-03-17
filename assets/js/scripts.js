b3cea8570f662f1f4902a43ce3cfc0280b5ab2f9
oterdiman <olivier.terdiman@gmail.com>
2023-03-17 10:25:36 +0100

    ajout meta noindex temporaire

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -60,9 +60,7 @@ window.addEventListener('DOMContentLoaded', event => {
     const message      = document.getElementById("message");
     const emailRegex   = new RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, "gm");
     let error          = true;
-    let errorMesage    = "";
-    //const phoneRegex   = new RegExp(/^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/, "gm");
-    //email.validity.typeMismatch || email.validity.patternMismatch
+    let errorMesage    = "Une erreur est survenue.";
 
     form.addEventListener("submit", (event) => {
 
@@ -71,21 +69,25 @@ window.addEventListener('DOMContentLoaded', event => {
             errorMesage = "Le champ nom doit avoir entre 3 et 150 caractères.";
             error = true;
             event.preventDefault();
+            document.getElementById("nameError").style.cssText = "display : block; visibility : visible;";
         } else {
             error = false;
             errorMesage = "";
+            document.getElementById("nameError").style.cssText = "display : none; visibility : hidden;";
         }
 
         const isValidEmail = emailRegex.test(email.value);
-        if(!isValidEmail) {
+        if(!isValidEmail || email.validity.typeMismatch || email.validity.patternMismatch || '' === email.value.trim()) {
             event.preventDefault();
-            console.log("email NOK");
+            console.log("email NOK : " + email.value);
             errorMesage = "Le champ email semble invalide, veuillez entrer une adresse mail valide.";
             error = true;
+            document.getElementById("emailError").style.cssText = "display : block; visibility : visible;";
         } else {
             console.log("email OK");
             error = false;
             errorMesage = "";
+            document.getElementById("emailError").style.cssText = "display : none; visibility : hidden;";
         }
 
         if (phone.validity.typeMismatch || phone.validity.patternMismatch || '' === phone.value.trim()) {
@@ -93,12 +95,14 @@ window.addEventListener('DOMContentLoaded', event => {
             console.log("phone NOK");
             errorMesage = "Le champ téléphone n/'est pas au bon format.";
             error = true;
+            document.getElementById("phoneError").style.cssText = "display : block; visibility : visible;";
         } else {
             console.log(phone.value + " = phone");
             console.log("phone OK");
             phone.setCustomValidity("");
             errorMesage = "";
             error = false;
+            document.getElementById("phoneError").style.cssText = "display : none; visibility : hidden;";
         }
 
         if (message.validity.typeMismatch || message.validity.patternMismatch || '' === message.value.trim()) {
@@ -106,13 +110,14 @@ window.addEventListener('DOMContentLoaded', event => {
             console.log("message NOK");
             errorMesage = "Le champ message doit comporter au minimum 15 caractères (max 850).";
             error = true;
+            document.getElementById("messageError").style.cssText = "display : block; visibility : visible;";
         } else {
             console.log("message OK");
             message.setCustomValidity("");
             errorMesage = "";
             error = false;
+            document.getElementById("messageError").style.cssText = "display : none; visibility : hidden;";
         }
-
 
         if( !error && "" === errorMesage) {
             console.log("NO ERRORS");
